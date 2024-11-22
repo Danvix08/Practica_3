1e897f11639caf023b15fb1b2196c56cd94d8143
David León <dlesen08gmail.com>
2024-11-22 21:15:11 +0100

    Codi 4 i 4c

---
+++ exercici4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int b = 3;
+    char c = 'a';
+    int result;
+
+    result = b * c;
+
+    return 0;
+}
---
+++ exercici4_c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int b = 5;
+    char c = 'a';
+    char result;
+
+    result = b * c;
+
+    return 0;
+}
